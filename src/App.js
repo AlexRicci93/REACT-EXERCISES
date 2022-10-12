552d124e1fe834cdad201282a3f2a01aea51fc54
Alessandro Ricci <alessandroricci@MBPdiAlessandro.homenet.telecomitalia.it>
2022-10-12 16:44:11 +0200

    events-03

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Hello } from './Hello'
 import { Welcome } from './Welcome'
 import { Counter } from './Counter'
 import { ClickCounter } from './ClickCounter'
+import { ClickTracker } from './ClickTracker'
 
 
 export class App extends REACT.Component {
@@ -16,6 +17,7 @@ export class App extends REACT.Component {
                 <Welcome name="John" age={29}/>
                 <Counter initialValue={0} incrementingBy={1} incrementInterval={1000}/>
                 <ClickCounter/>
+                <ClickTracker/>
             </div>
         )
     }
---
+++ src/ClickTracker.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+export class ClickTracker extends React.Component {
+
+
+    state =  {
+
+        name: '',
+
+    }
+
+
+    handleTrackerButton = (event) => {
+ 
+        this.setState(() => {
+            return {
+            name: event.target.name
+        }
+        })
+        
+
+    }
+
+    render() {
+
+        return (
+
+            <div>
+                <h1>{this.state.name}</h1>
+                <button name="ciao" onClick={this.handleTrackerButton}>1</button>
+                <button name="ciaooo" onClick={this.handleTrackerButton}>2</button>
+                <button name="tao" onClick={this.handleTrackerButton}>3</button>
+            </div>
+        )
+    }
+}
