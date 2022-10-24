a7c4b17bc48fdf27e606cb7e05462e0a17014cf4
Alessandro Ricci <alessandroricci@MBPdiAlessandro.homenet.telecomitalia.it>
2022-10-24 18:34:55 +0200

    react-router-04

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import { Welcome } from "./Welcome";
 import { Counter} from "./Counter";
 import { ShowGithubUser } from "./ShowGithubUser";
+import { Link } from "react-router-dom";
 
 
 
@@ -18,7 +19,11 @@ export class App extends React.Component {
                 <Route path="/counter" element={<Counter/>}/>   
                 <Route path="users/:username" element={<ShowGithubUser/>}/>
             </Routes>
-           
+        <div style={{display: 'flex', justifyContent:'space-around'}}>
+           <Link to="./" style={{textDecoration: 'none', color: 'maroon'}}><h2>Home</h2></Link>
+           <Link to="./counter" style={{textDecoration: 'none', color: 'maroon'}}><h2>Counter</h2></Link>
+           <Link to="users/:username" style={{textDecoration: 'none', color: 'maroon' }}><h2>User</h2></Link>
+        </div>
             </>
         )
     }
